df8793015a027c6dc98bb95f927be77a323aa827
arist0crab <BondareVarechka@yandex.ru>
2024-10-14 17:24:52 +0300

    Done laba_6_4

--- laba_6/laba_6_4.py
+++ laba_6/laba_6_4.py
@@ -23,29 +23,36 @@ for i in range(num_of_items):
                      'числом, попробуйте еще раз: ')
     arr.append(int(elem))
 
-
-def is_simple(n):
-    if n <= 0:
-        return False
-    if n % 2 == 0 or n == 1:
-        return n == 2
-    d = 3
-    while d * d <= n and n % d != 0:
-        d += 2
-    return d * d > n
-
-
+# Основной алгоритм поиска наибольшей последовательности
 current_length = 0
 max_length = 0
 current_start_index = -1
 max_start_index = -1
 previous = arr[0]
-previous_is_simple = is_simple(arr[0])
+# Проверяем, простой ли первый элемент
+if arr[0] <= 0:
+    previous_is_simple = False
+elif arr[0] % 2 == 0 or arr[0] == 1:
+    previous_is_simple = arr[0] == 2
+else:
+    d = 3
+    while d * d <= arr[0] and arr[0] % d != 0:
+        d += 2
+    previous_is_simple = d * d > arr[0]
 if previous_is_simple:
     current_length += 1
     current_start_index = 0
 for i in range(1, num_of_items):
-    current_is_simple = is_simple(arr[i])
+    # Проверяем, простой ли элемент
+    if arr[i] <= 0:
+        current_is_simple = False
+    elif arr[i] % 2 == 0 or arr[i] == 1:
+        current_is_simple = arr[i] == 2
+    else:
+        d = 3
+        while d * d <= arr[i] and arr[i] % d != 0:
+            d += 2
+        current_is_simple = d * d > arr[i]
     if current_is_simple and arr[i] < previous and previous_is_simple:
         current_length += 1
         previous_is_simple = True
@@ -64,6 +71,10 @@ for i in range(1, num_of_items):
 if max_length < current_length:
     max_length = current_length
     max_start_index = current_start_index
-print(arr[max_start_index:max_start_index + max_length])
-print('start index', max_start_index)
-print('length', max_length)
+
+# Вывод результатов на печать
+if max_start_index >= 0:
+    print('Наиболее длинная непрерывная убывающая последовательность простых чисел: ',
+          arr[max_start_index:max_start_index + max_length])
+else:
+    print('В данном списке не обнаружено искомых последовательностей')
