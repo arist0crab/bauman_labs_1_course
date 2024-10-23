6ef482e277b7c7249b8e183088160bbaa891e5fe
arist0crab <BondareVarechka@yandex.ru>
2024-10-23 14:27:57 +0300

    bugfix laba_7_4 part 1

--- laba_7/laba_7_4.py
+++ laba_7/laba_7_4.py
@@ -11,36 +11,25 @@
 num_of_items = input('Введите количество элементов списка: ')
 arr = list()
 
-# Защита от хомяков 1 - проверка количества элементов списка
+# Защита от хомяков - проверка количества элементов списка
 while not num_of_items.isdigit() or num_of_items == '0':
     num_of_items = input('Количество элементов списка должно '
                          'быть целым положительным числом, попробуйте еще раз: ')
 num_of_items = int(num_of_items)
-# Защита от хомяков 2 - проверка, что все элементы списка - целые числа
-for i in range(num_of_items):
-    elem = input('Введите элемент: ')
-    if len(elem) == 1 and elem.isdigit():
-        elem = int(elem)
-    arr.append(elem)
+
+# Создание списка строк
+arr = [input('>> ') for i in range(num_of_items)]
 
 # Основной цикл
-# Проходимся по всем элементам списка, складываем два подходящих значения,
-# следующее после этого значения обращаем в None, чтобы потом удалить
-for i in range(num_of_items - 1):
-    if arr[i] is None:
-        continue
-    if type(arr[i]) is int and type(arr[i + 1]) is int:
-        arr[i] += arr[i + 1]
-        arr[i] %= 10
-        arr[i + 1] = None
-# Удаляем все значение None (тот же алгоритм, что и в первом задании)
-insert_index = 0
-for elem in arr:
-    # Проверяем, подходит ли элемент под условие
-    if elem is not None:
-        arr[insert_index] = elem
-        insert_index += 1
-arr = arr[:insert_index]
+for i in range(num_of_items):
+    new = ''
+    for char in range(len(arr[i]) - 1):
+        if arr[i][char].isdigit() and arr[i][char + 1].isdigit():
+            new += f'{(int(arr[i][char]) + int(arr[i][char + 1])) % 10}'
+        else:
+            new += arr[i][char]
+    print(new)
+
 
 # Вывод полученного списка
 print(f'Полученный список: {arr}')
